77fbb2fd6be7dbcd6a5cd1c0adf32f00eb1d314c
bradysullivan <Brady.Sullivan@iwsinc.com>
2013-03-29 12:40:58 -0700

    Initial ident.py commit.

---
+++ danger/ident.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+class ident():
+  def __init__(self, nick, password, real, quit):
+    self.nick = nick
+    self.password = password
+    self.real = real
+    self.quit = quit
